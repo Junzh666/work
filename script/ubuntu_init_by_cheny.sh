1047114f9ad32619642b890b7d7f9c8e3a586f0a
Cheny <junzh666@gmail.com>
2023-05-18 22:16:36 +0800

    fix

--- script/ubuntu_init_by_cheny.sh
+++ script/ubuntu_init_by_cheny.sh
@@ -4,7 +4,7 @@
 # This script is used to init ubuntu system by myself. It will automatically install some softwares and configure some settings.
 # The script run as normal user, so you should not run it with sudo command. Sometimes the first 'sudo' command need input user's password,
 # so before you run this first script, you should run 'sudo echo' to input your password. 
-# Remember that you don't run this script with sudo command, because it will install some softwares and configure some settings for root.
+# Remember that you don't run this script with sudo command, because it will install some softwares and configure some settings for rootsudo.
 
 # ENV
 SSH_PUBLIC_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCjabJ4UW8Rx50NChKBoHzfxGPNF/caByQ2RPiZUQ69dAzXSxq63+SUhHho2t31LWq8MWy1FjRGuxJXiSju8NTJk5IPU+CLD6IqttZr/CgvHNbGXlZ/MAMuN68tHqKQag90kZDfBqeNlY2NizuYkhrT1Hvxu65hLZbEOrofFLg1KC82BNKwQqjcoLnbpMVi+8CDnEL9MqDA22St4p4K/X/FvOutsiaJ/+p1yQW+ejD6+AUpKSIZAz4YzqBls4sG8sPPlbYC/WPcupACQziqARXhD7WwrlS7jEgMkwvznhAl3InecsECvOx3K3/1Au4vCAAozAb5vbHVXASeJOVAG44KTx3amCbMhyNzvkFMndzlU8XZYojs8ZoARLQLLNmQHpjsw/T55cmQAtVkI/qUVp+gMoLSmL5tRE7xPDOjI9GuEu4CKFokA0I2quVV54aJ3RjM9TII5Xr+0ofGLwzobIw6Hga2oyfzHMoTnkE9logzIjPJRqi4IKf1UsyKf8KNsc8= cheny@=macbook-pro.local"
@@ -15,7 +15,7 @@ if [ ! -f /etc/apt/sources.list.bak ]; then
 fi
 sudo sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list
 sudo sed -i 's/security.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list
-apt update
+sudo apt update
 
 # config ssh with SSH_PUBLIC_KEY
 mkdir -p ~/.ssh
@@ -34,10 +34,10 @@ chmod 600 ~/.ssh/authorized_keys
 sudo sed -i 's/%sudo\tALL=(ALL:ALL) ALL/%sudo\tALL=(ALL:ALL) NOPASSWD:ALL/g' /etc/sudoers
 
 # install git
-apt install -y git curl wget
+sudo apt install -y git curl wget
 
 # install zsh and oh-my-zsh
-apt install -y zsh
+sudo apt install -y zsh
 chsh -s /bin/zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
@@ -51,8 +51,8 @@ sh ~/.vim_runtime/install_awesome_vimrc.sh
 sed -ie "s/^let g:ctrlp_map.*/\# &/" .vim_runtime/vimrcs/plugins_config.vim
 
 # disenble ssh password permission
-sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
-service sshd restart
+sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+sudo service sshd restart
 
 
 
